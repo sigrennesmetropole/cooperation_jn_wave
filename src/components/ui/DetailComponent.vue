2d6fbee0e842bd7523f7a102987a4cdb3ad2bb62
anaislet-c2c <anais.letellier@camptocamp.com>
2023-08-09 16:16:17 +0200

    refacto DetailComponent

--- src/components/ui/DetailComponent.vue
+++ src/components/ui/DetailComponent.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import { usePointsStore } from '@/stores/points'
 import { UiButtonWithTooltip } from '@sigrennesmetropole/cooperation_jn_common_ui'
-import ConformityIcon from '@/assets/icons/standard.svg'
-import SurveillanceIcon from '@/assets/icons/under-surveillance.svg'
-import NonConformingIcon from '@/assets/icons/improper.svg'
+import { computed } from 'vue'
 
 const pointStore = usePointsStore()
+
+const ConformityLabel = computed(() => {
+  if (pointStore.conformity == 'conform') {
+    return '/src/assets/icons/standard.svg'
+  } else if (pointStore.conformity == 'under surveillance') {
+    return '/src/assets/icons/under-surveillance.svg'
+  } else if (pointStore.conformity == 'non-conforming') {
+    return '/src/assets/icons/improper.svg'
+  } else {
+    return null
+  }
+})
 </script>
 
 <template>
@@ -33,15 +43,7 @@ const pointStore = usePointsStore()
       <p class="font-dm-sans font-bold text-lg leading-6 text-green-500">
         <span class="text-2xl">{{ pointStore.latest_value }}</span> V/m
       </p>
-      <img v-if="pointStore.conformity == 'conform'" :src="ConformityIcon" />
-      <img
-        v-if="pointStore.conformity == 'under surveillance'"
-        :src="SurveillanceIcon"
-      />
-      <img
-        v-if="pointStore.conformity == 'non-conforming'"
-        :src="NonConformingIcon"
-      />
+      <img :src="ConformityLabel ? ConformityLabel : undefined" />
     </div>
     <div
       v-if="pointStore.status == 'MAINTENANCE'"
