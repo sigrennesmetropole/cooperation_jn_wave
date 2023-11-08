b0fec777e8b21330aea7308c6d3e7a291fcbb2e8
Ismail Sunni <imajimatika@gmail.com>
2023-11-08 16:36:31 +0700

    Move buttonwithtooltip.

--- src/components/ui/DetailComponent.vue
+++ src/components/ui/DetailComponent.vue
@@ -27,27 +27,36 @@ const ConformityLabel = computed(() => {
       <p class="font-dm-sans font-medium text-base text-neutral-600">
         {{ pointStore.address }}
       </p>
-      <UiButtonWithTooltip
-        widthButton="4"
-        heightButton="4"
-        zIndex="z-10"
-        text="La mesure enregistrée respecte la limite réglementaire d’exposition la plus restrictive et le seuil d’alerte maximal."
-        widthBoxText="w-[300px]"
-        positionTop="4px"
-      ></UiButtonWithTooltip>
+      <p class="font-dm-sans font-normal text-sm text-neutral-400">
+        Mesure effectuée le {{ pointStore.lastCom }}
+      </p>
     </div>
-    <p class="font-dm-sans font-normal text-sm text-neutral-400">
-      Mesure effectuée le {{ pointStore.lastCom }}
-    </p>
     <div
       v-if="pointStore.status == 'ONLINE'"
       class="flex flex-row justify-between"
     >
-      <p class="font-dm-sans font-bold text-lg leading-6 text-green-500">
-        <span class="text-2xl">{{ pointStore.latest_value }}</span> V/m
-      </p>
+      <div class="flex flex-row">
+        <div class="flex-1 flex">
+          <p class="font-dm-sans font-bold text-lg leading-6 text-green-500">
+            <span class="text-2xl">{{ pointStore.latest_value }}</span> V/m
+          </p>
+        </div>
+
+        <div class="relative">
+          <UiButtonWithTooltip
+            widthButton="4"
+            heightButton="4"
+            zIndex="z-10"
+            text="La mesure enregistrée respecte la limite réglementaire d’exposition la plus restrictive et le seuil d’alerte maximal."
+            widthBoxText="w-[300px]"
+            positionTop="4px"
+          ></UiButtonWithTooltip>
+        </div>
+      </div>
+
       <img :src="ConformityLabel ? ConformityLabel : undefined" />
     </div>
+
     <div
       v-if="pointStore.status == 'MAINTENANCE'"
       class="flex flex-row justify-between"
