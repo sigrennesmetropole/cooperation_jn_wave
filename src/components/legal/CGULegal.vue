bc04dfc723af4fdb3dc3fa0a48176ebdd89d2cc8
Ismail Sunni <imajimatika@gmail.com>
2023-11-16 17:38:38 +0700

    Add new CGU legal page.

---
+++ src/components/legal/CGULegal.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+<template>
+  <div
+    class="flex flex-col gap-12 w-[880px] h-fit bg-white rounded-xl px-8 py-16 mx-auto mt-32 mb-[87px]"
+  >
+    <h1
+      class="flex flex-column align-start text-[44px] leading-[48px] lmb-12 font-bold"
+    >
+      CGU
+    </h1>
+    <div class="text-base font-normal">
+      <p>L'application n'utilise pas de cookie.</p>
+    </div>
+  </div>
+</template>
